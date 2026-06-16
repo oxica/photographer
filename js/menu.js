77dee5d3785410f08bc6e9353a5a8627e95a52a7
Oksana Banshchykova <88445174+oxica@users.noreply.github.com>
2026-06-16 18:54:15 +0200

    22

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,36 @@
+// (() => {
+//   const refs = {
+//     openMenuBtn: document.querySelector("[data-menu-open]"),
+//     closeMenuBtn: document.querySelector("[data-menu-close]"),
+//     menu: document.querySelector("[data-menu]"),
+//   };
+
+//   refs.openMenuBtn.addEventListener("click", toggleMenu);
+//   refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+//   function toggleMenu() {
+//     refs.menu.classList.toggle("is-hidden");
+//   }
+// })();
+
 (() => {
   const refs = {
     openMenuBtn: document.querySelector("[data-menu-open]"),
     closeMenuBtn: document.querySelector("[data-menu-close]"),
     menu: document.querySelector("[data-menu]"),
+    // Знаходимо всі якірні посилання всередині нашого меню
+    menuLinks: document.querySelectorAll("[data-menu] a"), 
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
   refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+  // Перебираємо кожне посилання і кажемо: "при кліку — сховай меню"
+  refs.menuLinks.forEach(link => {
+    link.addEventListener("click", () => {
+      refs.menu.classList.add("is-hidden");
+    });
+  });
 
   function toggleMenu() {
     refs.menu.classList.toggle("is-hidden");
